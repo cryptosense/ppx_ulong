820a4a184fe852903d1633ee57886ed96e610299
Nathan <nathan@cryptosense.com>
2017-12-12 15:19:01 +0100

    Drop tests' dependency over ppx_deriving

--- test/test.ml
+++ test/test.ml
@@ -2,10 +2,25 @@ open OUnit2
 
 let (>:=) s test = s >:: test s
 
+let show_string = Printf.sprintf "%S"
+
+let equal_string_res s s' =
+  let open Result in
+  match s, s' with
+    | Ok s, Ok s' -> String.equal s s'
+    | Error s, Error s' -> String.equal s s'
+    | Ok _, Error _
+    | Error _, Ok _
+      -> false
+
+let show_string_res = function
+  | Result.Ok s -> Printf.sprintf "Ok %s" @@ show_string s
+  | Result.Error s -> Printf.sprintf "Error %s" @@ show_string s
+
 let test_strip_underscores =
   let test expected s ctxt =
     let actual = Ppx_ulong_lib.strip_underscores s in
-    assert_equal ~ctxt ~cmp:[%eq: string] ~printer:[%show: string] expected actual
+    assert_equal ~ctxt ~cmp:String.equal ~printer:show_string expected actual
   in
   "strip_underscores" >:::
   [ "Empty" >:: test "" ""
@@ -16,11 +31,7 @@ let test_strip_underscores =
 let test_ulong_lit_string_of_string =
   let test expected s ctxt =
     let actual = Ppx_ulong_lib.ulong_lit_string_of_string s in
-    assert_equal ~ctxt
-      ~cmp:[%eq: (string, string) Result.result]
-      ~printer:[%show: (string, string) Result.result]
-      expected
-      actual
+    assert_equal ~ctxt ~cmp:equal_string_res ~printer:show_string_res expected actual
   in
   let open Result in
   "ulong_lit_string_of_string" >:::
